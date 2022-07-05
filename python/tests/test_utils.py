a27eea3eca6b489ec56c1a7e0d306e530ec4cadc
Abhinav Neelakantan <abhinav.neelakantan@factset.com>
2022-07-05 11:17:05 -0400

    chore(update): update based on code review comments

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -12,7 +12,7 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 class V2StachUtilsTests(unittest.TestCase):
     def test_stach_utils_v2(self):
         column_values = ["Port.+Weight","Bench.+Weight","Bench.+Weight","Bench.+Weight"]
-        file = os.path.join(ROOT_DIR, "resources", "V2StachUtils.json")
+        file = os.path.join(ROOT_DIR, "resources", "V2StachWithCompressedColumn.json")
         with open(file) as f:
             data = json.load(f)
             decompressed_data = StachUtilities.decompress_column(data["data"]["columns"]["col1"])
