e0e58790a16e8ae1413346073353e881649e010e
Justin Chen <109162763+justinchen0001@users.noreply.github.com>
2022-10-20 09:21:34 -0400

    using jsonlite in tests rather than rjson (#39)

--- R/factset.protobuf.stachextensions/tests/testthat/test-StachV2DecompressTests.R
+++ R/factset.protobuf.stachextensions/tests/testthat/test-StachV2DecompressTests.R
@@ -1,24 +1,22 @@
-
 source("../test_constants.R")
-library(rjson)
 
 testthat::test_that("column organized utilities, get primary table ids",
 {
   filePath = './../../inst/testdata/V2ColumnOrganizedCompressed.json'
-  compressed <- rjson::fromJSON(file=filePath, simplify=TRUE)
+  compressed <- jsonlite::read_json(path=filePath, auto_unbox=TRUE)
 
   ids <- factset.protobuf.stachextensions::V2ColumnOrganizedStachUtilities$
     public_methods$
     GetPrimaryTableIds(compressed)
 
   expect_equal(length(ids), 1, info= "Length of ids should be 1")
-  expect_equal(ids[1], "a649ec50-7e58-443d-b791-1340e9eebf24", info = "Primary table id should be a649ec50-7e58-443d-b791-1340e9eebf24")
+  expect_equal(ids[[1]], "a649ec50-7e58-443d-b791-1340e9eebf24", info = "Primary table id should be a649ec50-7e58-443d-b791-1340e9eebf24")
 })
 
 testthat::test_that("column organized utilities, decompress",
 {
   filePath = './../../inst/testdata/V2ColumnOrganizedCompressed.json'
-  package <- rjson::fromJSON(file=filePath, simplify=TRUE)
+  package <-jsonlite::read_json(path=filePath, auto_unbox=TRUE)
 
   utility <- factset.protobuf.stachextensions::V2ColumnOrganizedStachUtilities$new()
 
@@ -26,7 +24,7 @@ testthat::test_that("column organized utilities, decompress",
 
   primaryTableId <- "a649ec50-7e58-443d-b791-1340e9eebf24"
   actualData <- package[["tables"]][[primaryTableId]][["data"]][["columns"]][["1"]][["values"]]
-  
+
   expectedData <- c(list(NULL, NULL, NULL), "Americas",
             "Asia Pacific", "Europe", "Middle East and Africa",
             list(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL))
